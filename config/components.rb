08e25bb501246a013ef367858afdbf1c0b99e0a7
xana68 <alex@glyph.fr>
2015-04-21 17:44:16 +0200

    comment componont.rb

--- config/components.rb
+++ config/components.rb
@@ -3,19 +3,19 @@ Para.components.draw do
   # You can translate the section title at: components.section.menu
   #
 
-  section :menu do
-    component :form_text,        :crud, model_type: FormTest
-  end
+  # section :menu do
+  #   component :form_text,        :crud, model_type: FormTest
+  # end
   
-  section :ressources do
+  # section :ressources do
   
-   # Create a component of type :crud in the the section :menu, with the
-   # identifier :pages, with the model 'Page' as the target type and
-   # namespacing it's managed to resources to the component
-   #
-   # component :pages, :crud, model_type: 'Page', namespaced: true
+  #  # Create a component of type :crud in the the section :menu, with the
+  #  # identifier :pages, with the model 'Page' as the target type and
+  #  # namespacing it's managed to resources to the component
+  #  #
+  #  # component :pages, :crud, model_type: 'Page', namespaced: true
 
-   component :categories,        :crud, model_type: Category
-   component :ressources,        :crud, model_type: Ressource
-  end
+  #  component :categories,        :crud, model_type: Category
+  #  component :ressources,        :crud, model_type: Ressource
+  # end
 end
